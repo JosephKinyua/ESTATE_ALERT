41907a7aadf9362497e9caa089b3f1265be3b089
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-24 07:31:14 +0300

    add a model location that have a functionality save

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+
