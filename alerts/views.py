1cbec3c40afed5abfece7564d48d3e383e56ad8a
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-24 19:37:41 +0300

    make import for the views

--- alerts/views.py
+++ alerts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Post, Business, Neighborhood, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .form import UserForm, ProfileForm, BusinessForm, PostForm
 # Create your views here.
 def home(request):
     return render(request,'home.html',)
