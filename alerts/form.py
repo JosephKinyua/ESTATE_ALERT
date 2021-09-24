01cdb3e48fec353512262ea1ac416d7e1185c3dd
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-24 19:34:01 +0300

    define the userform

--- alerts/form.py
+++ alerts/form.py
@@ -2,3 +2,8 @@
 from django import forms
 from .models import Business, Post, Profile
 from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
