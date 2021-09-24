29d28fc1dc17f7ef79a0472ce4c6da4bafadf3b5
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-24 16:31:22 +0300

    add create user and save user functionality

--- alerts/models.py
+++ alerts/models.py
@@ -48,3 +48,16 @@ class Profile(models.Model):
     profilePic = models.ImageField(upload_to='userProfile/', default='userProfile/test.png')
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+        Profile.objects.create(username=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+    def __str__(self):
+        return self.username
