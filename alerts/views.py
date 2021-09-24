8a89096d10a7cd63a8550497edc9f3c26cd0ac70
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-25 00:11:36 +0300

    add a index view for the page

--- alerts/views.py
+++ alerts/views.py
@@ -3,6 +3,27 @@ from .models import Post, Business, Neighborhood, Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .form import UserForm, ProfileForm, BusinessForm, PostForm
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'home.html',)
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    if request.user.profile.neighborhood == None:
+        messages.success(request, 'Please fill out you Neighborhood')
+        return redirect('uprofile')
+    else:
+        neighdetails = Neighborhood.objects.get(
+            name=request.user.profile.neighborhood)
+        businesses = Business.objects.filter(
+            neighborhood=request.user.profile.neighborhood)
+        stories = Post.objects.filter(
+            neighborhood=request.user.profile.neighborhood)
+
+        params = {
+            'neighdetails': neighdetails,
+            'businesses': businesses,
+            'stories': stories,
+        }
+        return render(request, 'index.html', params)
