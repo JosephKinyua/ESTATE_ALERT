6e09282cc34793949bdd4a916680ef5294f59954
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-24 16:32:52 +0300

    add  post_save and receiver personality

--- alerts/models.py
+++ alerts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.db.models import Q
 
 # Create your models here.
@@ -52,7 +54,7 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
-        Profile.objects.create(username=instance)
+            Profile.objects.create(username=instance)
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
