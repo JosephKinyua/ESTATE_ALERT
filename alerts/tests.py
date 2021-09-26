285d79bf0bd770fe73e70636f596a8192b0ef9dd
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-09-26 21:02:59 +0300

    this implements the test for neighborhood

--- alerts/tests.py
+++ alerts/tests.py
@@ -18,3 +18,33 @@ class TestLocation(TestCase):
 
 
 
+class TestNeighborhood(TestCase):
+    def setUp(self):
+        self.location = Location.objects.create(location='Machakos')
+        self.hood = Neighborhood.objects.create(name='Kisumu Ndogo', location=self.location, policehelpline=2, hospitalhelpline=2, occupants=8)
+
+
+    def tearDown(self):
+        Location.objects.all().delete()
+        Neighborhood.objects.all().delete()
+
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.hood, Neighborhood))
+
+    def test_deletehood(self):
+        self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+        self.assertEqual(len(Neighborhood.objects.all()),2)
+        Neighborhood.delete_neigborhood(self.hood.id)
+        self.assertEqual(len(Neighborhood.objects.all()),1)
+
+    def test_findneighborhood(self):
+        searchterm = 'Embakasi'
+        self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+        results = Neighborhood.find_neigborhood(searchterm)
+        self.assertTrue(len(results), 1)
+
+    def test_updateneighbor(self):
+        self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+        Neighborhood.update_neighborhood(self.hood2.id, name='kajiado')
+        updated = Neighborhood.objects.get(id = self.hood2.id)
+        self.assertEqual(updated.name, 'kajiado')
